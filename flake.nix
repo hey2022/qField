3f3410003b7440ba266f7dc4be13a45d3a89d4c8
Yiheng He <yiheng.he@proton.me>
2025-01-15 18:52:31 +0800

    build: use pyproject.toml with uv

--- flake.nix
+++ flake.nix
@@ -4,45 +4,53 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pyproject-nix = {
+      url = "github:nix-community/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
+    pyproject-nix,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        overlays = [
-          (final: prev: {
-            python3 = prev.python3.override {
-              packageOverrides = python-final: python-prev: {
-                pygame = python-prev.pygame.overridePythonAttrs (old: {
-                  env.PYGAME_DETECT_AVX2 = "1";
-                });
-              };
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [
+        (final: prev: {
+          python3 = prev.python3.override {
+            packageOverrides = _: python-prev: {
+              pygame = python-prev.pygame.overridePythonAttrs (old: {
+                env.PYGAME_DETECT_AVX2 = "1";
+              });
             };
-          })
+          };
+        })
+      ];
+
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+
+      # Setup from pyproject.toml
+      python = pkgs.python3;
+      project = pyproject-nix.lib.project.loadPyproject {
+        projectRoot = ./.;
+      };
+      python-env = python.withPackages (project.renderers.withPackages {
+        inherit python;
+      });
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          python-env
+
+          basedpyright
+          uv
+          ruff
+          git
         ];
-
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-      in {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            (python3.withPackages (python-packages:
-              with python-packages; [
-                pygame
-
-                pip
-                pytest
-              ]))
-            basedpyright
-            ruff
-            git
-          ];
-        };
-      }
-    );
+      };
+    });
 }
