bf664c0803262fe6068a9d344fc2a121762e3cba
Yiheng He <yiheng.he@proton.me>
2025-05-21 11:22:26 +0800

    flake: remove pyproject-nix

--- flake.nix
+++ flake.nix
@@ -4,54 +4,44 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    pyproject-nix = {
-      url = "github:nix-community/pyproject.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
-    pyproject-nix,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [
-        (final: prev: {
-          python3 = prev.python3.override {
-            packageOverrides = _: python-prev: {
-              pygame = python-prev.pygame.overridePythonAttrs (old: {
-                env.PYGAME_DETECT_AVX2 = "1";
-              });
-            };
-          };
-        })
+      pkgs = import nixpkgs {inherit system;};
+      system-libraries = with pkgs; [
+        SDL2
+        SDL2_image
+        SDL2_mixer
+        SDL2_ttf
+        freetype
+        libjpeg
+        libpng
+        portmidi
+        stdenv.cc.cc.lib
+        xorg.libX11
+        xorg.libXext
+        xorg.libXrandr
+        zlib
       ];
-
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
-
-      # Setup from pyproject.toml
-      python = pkgs.python3;
-      project = pyproject-nix.lib.project.loadPyproject {
-        projectRoot = ./.;
-      };
-      python-env = python.withPackages (project.renderers.withPackages {
-        inherit python;
-      });
     in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
-          python-env
-
           basedpyright
           git
           pre-commit
+          python3
           ruff
           uv
         ];
+
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath system-libraries}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+        '';
       };
     });
 }
