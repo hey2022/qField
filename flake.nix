ed86dd68674398545e70fd3d19a6d6684b5344ee
Yiheng He <yiheng.he@proton.me>
2025-05-22 13:38:16 +0800

    refactor: move python into python directory
    - Add root flake.nix
    - Add root .gitignore

--- flake.nix
+++ flake.nix
@@ -13,35 +13,12 @@
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
-      system-libraries = with pkgs; [
-        SDL2
-        SDL2_image
-        SDL2_mixer
-        SDL2_ttf
-        freetype
-        libjpeg
-        libpng
-        portmidi
-        stdenv.cc.cc.lib
-        xorg.libX11
-        xorg.libXext
-        xorg.libXrandr
-        zlib
-      ];
     in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
-          basedpyright
-          git
           pre-commit
-          python3
-          ruff
-          uv
+          git
         ];
-
-        shellHook = ''
-          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath system-libraries}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
-        '';
       };
     });
 }
