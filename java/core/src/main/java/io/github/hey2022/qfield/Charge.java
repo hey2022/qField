58dbf3684a0aaa16fd9a6703929814829cad0e65
Yiheng He <yiheng.he@proton.me>
2025-05-28 19:43:13 +0800

    style: format java with google-java-format

--- java/core/src/main/java/io/github/hey2022/qfield/Charge.java
+++ java/core/src/main/java/io/github/hey2022/qfield/Charge.java
@@ -5,120 +5,111 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
-import org.nd4j.linalg.api.buffer.DataType;
-import org.nd4j.linalg.api.iter.NdIndexIterator;
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.factory.Nd4j;
-import org.nd4j.linalg.ops.transforms.Transforms;
-
 public class Charge {
-    private static final float[] c = {
-            0.03809449742241219545697532230863756534060f,
-            0.1452987161169137492940200726606637497442f,
-            0.2076276957255412507162056113249882065158f,
-            0.4359097036515261592231548624010651844006f,
-            -0.6538612258327867093807117373907094120024f,
-            0.4359097036515261592231548624010651844006f,
-            0.2076276957255412507162056113249882065158f,
-            0.1452987161169137492940200726606637497442f,
-    };
-    private static final float[] d = {
-            0.09585888083707521061077150377145884776921f,
-            0.2044461531429987806805077839164344779763f,
-            0.2170703479789911017143385924306336714532f,
-            -0.01737538195906509300561788011852699719871f,
-            -0.01737538195906509300561788011852699719871f,
-            0.2170703479789911017143385924306336714532f,
-            0.2044461531429987806805077839164344779763f,
-            0.09585888083707521061077150377145884776921f,
-    };
-    private final float ELEMENTAL_CHARGE = 1.602e-19f;
-    private final float K = 8.988e9f;
-    private final float PROTON_MASS = 1.673e-27f;
-    private final float SCALE = 1e-5f;
-    public final float RADIUS = 10;
-    private final float HEIGHT2 = 8.1e-17f;
-    private int i = 0;
+  private static final float[] c = {
+    0.03809449742241219545697532230863756534060f,
+    0.1452987161169137492940200726606637497442f,
+    0.2076276957255412507162056113249882065158f,
+    0.4359097036515261592231548624010651844006f,
+    -0.6538612258327867093807117373907094120024f,
+    0.4359097036515261592231548624010651844006f,
+    0.2076276957255412507162056113249882065158f,
+    0.1452987161169137492940200726606637497442f,
+  };
+  private static final float[] d = {
+    0.09585888083707521061077150377145884776921f,
+    0.2044461531429987806805077839164344779763f,
+    0.2170703479789911017143385924306336714532f,
+    -0.01737538195906509300561788011852699719871f,
+    -0.01737538195906509300561788011852699719871f,
+    0.2170703479789911017143385924306336714532f,
+    0.2044461531429987806805077839164344779763f,
+    0.09585888083707521061077150377145884776921f,
+  };
+  private final float ELEMENTAL_CHARGE = 1.602e-19f;
+  private final float K = 8.988e9f;
+  private final float PROTON_MASS = 1.673e-27f;
+  private final float SCALE = 1e-5f;
+  public final float RADIUS = 10;
+  private final float HEIGHT2 = 8.1e-17f;
+  private int i = 0;
 
-    private Vector2 position;
-    private Vector2 force;
-    private Vector2 acceleration;
-    private Vector2 velocity;
-    private float mass;
-    private boolean fixed;
-    private Color color;
+  private Vector2 position;
+  private Vector2 force;
+  private Vector2 acceleration;
+  private Vector2 velocity;
+  private float mass;
+  private boolean fixed;
+  private Color color;
 
-    private float charge;
+  private float charge;
 
-    public Charge(float x, float y, float charge, boolean fixed, float mass) {
-        this.reset(x, y);
-        this.mass = mass * this.PROTON_MASS;
-        this.fixed = fixed;
-        this.color = charge > 0 ? Color.RED : Color.BLUE;
-        this.charge = charge * this.ELEMENTAL_CHARGE;
+  public Charge(float x, float y, float charge, boolean fixed, float mass) {
+    this.reset(x, y);
+    this.mass = mass * this.PROTON_MASS;
+    this.fixed = fixed;
+    this.color = charge > 0 ? Color.RED : Color.BLUE;
+    this.charge = charge * this.ELEMENTAL_CHARGE;
+  }
+
+  public void reset(float x, float y) {
+    position = new Vector2(x, y);
+    velocity = new Vector2(0, 0);
+    acceleration = new Vector2(0, 0);
+    force = new Vector2(0, 0);
+  }
+
+  public void applyForce(Vector2 force) {
+    this.force = force;
+    this.acceleration = force.scl(1f / this.mass);
+  }
+
+  public void updateForce(Array<Charge> charges) {
+    applyForce(superposition(charges));
+  }
+
+  public void update(float time_step) {
+    if (fixed) return;
+    velocity.add(acceleration.scl(time_step * d[i]));
+    position.add(velocity.scl(time_step * c[i]));
+    i++;
+    i %= 8; // 8th-order
+  }
+
+  public float getCharge() {
+    return charge;
+  }
+
+  public Vector2 getPos() {
+    return position;
+  }
+
+  public Vector2 superposition(Array<Charge> charges) {
+    if (charges == null || charges.size == 0) {
+      return new Vector2(0, 0);
     }
 
-    public void reset(float x, float y) {
-        position = new Vector2(x, y);
-        velocity = new Vector2(0, 0);
-        acceleration = new Vector2(0, 0);
-        force = new Vector2(0, 0);
+    Vector2[] positions = new Vector2[charges.size];
+    float[] q = new float[charges.size];
+    for (int i = 0; i < charges.size; i++) {
+      Charge charge = charges.get(i);
+      positions[i] = charge.getPos();
+      q[i] = charge.getCharge();
     }
 
-    public void applyForce(Vector2 force) {
-        this.force = force;
-        this.acceleration = force.scl(1f / this.mass);
+    Vector2 force = new Vector2(0, 0);
+    for (int j = 0; j < positions.length; j++) {
+      Vector2 r = positions[j].cpy().sub(position).scl(SCALE);
+      float r2 = r.len2();
+      if (r2 == 0) continue; // Skip self interaction
+      float f = (K * q[j] * charge * r.len()) / (float) Math.pow((r2 + HEIGHT2), 1.5);
+      force.add(r.nor().scl(f));
     }
+    return force.scl(-1);
+  }
 
-    public void updateForce(Array<Charge> charges) {
-        applyForce(superposition(charges));
-    }
-
-    public void update(float time_step) {
-        if (fixed)
-            return;
-        velocity.add(acceleration.scl(time_step * d[i]));
-        position.add(velocity.scl(time_step * c[i]));
-        i++;
-        i %= 8; // 8th-order
-    }
-
-    public float getCharge() {
-        return charge;
-    }
-
-    public Vector2 getPos() {
-        return position;
-    }
-
-    public Vector2 superposition(Array<Charge> charges) {
-        if (charges == null || charges.size == 0) {
-            return new Vector2(0, 0);
-        }
-
-        Vector2[] positions = new Vector2[charges.size];
-        float[] q = new float[charges.size];
-        for (int i = 0; i < charges.size; i++) {
-            Charge charge = charges.get(i);
-            positions[i] = charge.getPos();
-            q[i] = charge.getCharge();
-        }
-
-        Vector2 force = new Vector2(0, 0);
-        for (int j = 0; j < positions.length; j++) {
-            Vector2 r = positions[j].cpy().sub(position).scl(SCALE);
-            float r2 = r.len2();
-            if (r2 == 0)
-                continue; // Skip self interaction
-            float f = (K * q[j] * charge * r.len()) / (float) Math.pow((r2 + HEIGHT2), 1.5);
-            force.add(r.nor().scl(f));
-        }
-        return force.scl(-1);
-    }
-
-    public void draw(ShapeRenderer shape) {
-        shape.setColor(color);
-        shape.circle(position.x, position.y, RADIUS);
-    }
-
+  public void draw(ShapeRenderer shape) {
+    shape.setColor(color);
+    shape.circle(position.x, position.y, RADIUS);
+  }
 }
--- java/core/src/main/java/io/github/hey2022/qfield/Main.java
+++ java/core/src/main/java/io/github/hey2022/qfield/Main.java
@@ -1,10 +1,12 @@
 package io.github.hey2022.qfield;
 
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Application.ApplicationType;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL32;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -12,153 +14,140 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL32;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.InputProcessor;
 
-/**
- * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
- * platforms.
- */
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main implements ApplicationListener {
 
-    static final float MIN_WORLD_WIDTH = 800;
-    static final float MIN_WORLD_HEIGHT = 800;
+  static final float MIN_WORLD_WIDTH = 800;
+  static final float MIN_WORLD_HEIGHT = 800;
 
-    private ShapeRenderer shapeRender;
-    private SpriteBatch batch;
-    private OrthographicCamera camera;
-    private OrthographicCamera hudCamera;
-    private Viewport viewport;
-    private Viewport hudViewport;
-    private double camSpeed;
-    private Vector2 touchPos;
-    private BitmapFont font;
+  private ShapeRenderer shapeRender;
+  private SpriteBatch batch;
+  private OrthographicCamera camera;
+  private OrthographicCamera hudCamera;
+  private Viewport viewport;
+  private Viewport hudViewport;
+  private double camSpeed;
+  private Vector2 touchPos;
+  private BitmapFont font;
 
-    private Array<Charge> charges;
-    private Charge charge;
+  private Array<Charge> charges;
+  private Charge charge;
 
-    @Override
-    public void create() {
-        // Prepare your application here.
-        if (Gdx.app.getType() != ApplicationType.HeadlessDesktop) {
-            shapeRender = new ShapeRenderer();
-            batch = new SpriteBatch();
-            font = new BitmapFont();
-        }
-
-        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getWidth());
-        viewport = new ScreenViewport(camera);
-        camSpeed = 200;
-
-        hudCamera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getWidth());
-        hudViewport = new ScreenViewport(hudCamera);
-
-        touchPos = new Vector2();
-
-        charge = new Charge(0, 0, 1, false, 1);
-        charges = new Array<Charge>();
-
+  @Override
+  public void create() {
+    // Prepare your application here.
+    if (Gdx.app.getType() != ApplicationType.HeadlessDesktop) {
+      shapeRender = new ShapeRenderer();
+      batch = new SpriteBatch();
+      font = new BitmapFont();
     }
 
-    @Override
-    public void resize(int width, int height) {
-        // Resize your application here. The parameters represent the new window size.
-        viewport.update(width, height, true);
-        hudViewport.update(width, height, true);
+    camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getWidth());
+    viewport = new ScreenViewport(camera);
+    camSpeed = 200;
+
+    hudCamera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getWidth());
+    hudViewport = new ScreenViewport(hudCamera);
+
+    touchPos = new Vector2();
+
+    charge = new Charge(0, 0, 1, false, 1);
+    charges = new Array<Charge>();
+  }
+
+  @Override
+  public void resize(int width, int height) {
+    // Resize your application here. The parameters represent the new window size.
+    viewport.update(width, height, true);
+    hudViewport.update(width, height, true);
+  }
+
+  @Override
+  public void render() {
+    // Draw your application here.
+    input();
+    logic();
+    if (shapeRender != null) {
+      draw();
+    }
+  }
+
+  private void draw() {
+    ScreenUtils.clear(Color.WHITE);
+    camera.update();
+    hudCamera.update();
+    shapeRender.setProjectionMatrix(camera.combined);
+    batch.setProjectionMatrix(hudCamera.combined);
+    Gdx.gl.glClear(GL32.GL_COLOR_BUFFER_BIT);
+
+    viewport.apply();
+    shapeRender.begin(ShapeType.Filled);
+    charge.draw(shapeRender);
+    for (Charge q : charges) {
+      q.draw(shapeRender);
+    }
+    shapeRender.end();
+
+    viewport.apply();
+    hudCamera.update();
+    batch.setProjectionMatrix(hudCamera.combined);
+    batch.begin();
+    font.setColor(Color.BLACK);
+    font.getData().setScale(2.0f);
+    font.draw(batch, "FPS=" + Gdx.graphics.getFramesPerSecond(), 0, hudCamera.viewportHeight);
+    batch.end();
+  }
+
+  private void logic() {
+    camera.update();
+    float delta = Gdx.graphics.getDeltaTime();
+    for (int i = 0; i < 8; i++) {
+      charge.updateForce(charges);
+      charge.update(delta);
+      // System.out.println(charge.getPos());
+    }
+  }
+
+  private void input() {
+    float displacement = (float) (camSpeed * Gdx.graphics.getDeltaTime());
+    if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+      camera.translate(-displacement, 0, 0);
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+      camera.translate(displacement, 0, 0);
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+      camera.translate(0, displacement, 0);
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+      camera.translate(0, -displacement, 0);
     }
 
-    @Override
-    public void render() {
-        // Draw your application here.
-        input();
-        logic();
-        if (shapeRender != null) {
-            draw();
-        }
+    if (Gdx.input.isTouched()) {
+      touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+      viewport.unproject(touchPos);
+      charges.add(new Charge(touchPos.x, touchPos.y, 1, true, 1));
+      System.out.println(touchPos);
     }
 
-    private void draw() {
-        ScreenUtils.clear(Color.WHITE);
-        camera.update();
-        hudCamera.update();
-        shapeRender.setProjectionMatrix(camera.combined);
-        batch.setProjectionMatrix(hudCamera.combined);
-        Gdx.gl.glClear(GL32.GL_COLOR_BUFFER_BIT);
+    if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {}
+  }
 
-        viewport.apply();
-        shapeRender.begin(ShapeType.Filled);
-        charge.draw(shapeRender);
-        for (Charge q : charges) {
-            q.draw(shapeRender);
-        }
-        shapeRender.end();
+  @Override
+  public void pause() {
+    // Invoked when your application is paused.
+  }
 
-        viewport.apply();
-        hudCamera.update();
-        batch.setProjectionMatrix(hudCamera.combined);
-        batch.begin();
-        font.setColor(Color.BLACK);
-        font.getData().setScale(2.0f);
-        font.draw(batch, "FPS=" + Gdx.graphics.getFramesPerSecond(), 0, hudCamera.viewportHeight);
-        batch.end();
+  @Override
+  public void resume() {
+    // Invoked when your application is resumed after pause.
+  }
 
-    }
-
-    private void logic() {
-        camera.update();
-        float delta = Gdx.graphics.getDeltaTime();
-        for (int i = 0; i < 8; i++) {
-            charge.updateForce(charges);
-            charge.update(delta);
-            // System.out.println(charge.getPos());
-        }
-    }
-
-    private void input() {
-        float displacement = (float) (camSpeed * Gdx.graphics.getDeltaTime());
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            camera.translate(-displacement, 0, 0);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            camera.translate(displacement, 0, 0);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            camera.translate(0, displacement, 0);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            camera.translate(0, -displacement, 0);
-        }
-
-        if (Gdx.input.isTouched()) {
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
-            viewport.unproject(touchPos);
-            charges.add(new Charge(touchPos.x, touchPos.y, 1, true, 1));
-            System.out.println(touchPos);
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-
-        }
-    }
-
-    @Override
-    public void pause() {
-        // Invoked when your application is paused.
-    }
-
-    @Override
-    public void resume() {
-        // Invoked when your application is resumed after pause.
-    }
-
-    @Override
-    public void dispose() {
-        // Destroy application's resources here.
-    }
-
+  @Override
+  public void dispose() {
+    // Destroy application's resources here.
+  }
 }
